68efa17f28b2f93086ca4165ef98cd735cd7bdeb
Cong Wang <wangcongrobot@gmail.com>
2021-01-12 00:02:09 +0000

    update rexrov2_ur3 model

--- robosuite/utils/input_utils.py
+++ robosuite/utils/input_utils.py
@@ -127,6 +127,7 @@ def choose_robots(exclude_bimanual=False):
         "Kinova3",
         "IIWA",
         "UR5e",
+        "RexROV2UR3",
     }
 
     # Add Baxter if bimanual robots are not excluded
